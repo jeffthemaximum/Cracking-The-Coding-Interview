181ae172e1bdd5ee9e021eace0de9421f466f1b8
jeffthemaximum <frey.maxim@gmail.com>
2015-08-14 16:35:17 -0400

    first commit

---
+++ one_one/src/one_one/one_one.java
@@ -0,0 +1,24 @@
+// Implement an algorithm to determine if a string has all unique characters.
+package one_one;
+import java.util.HashMap;
+
+public class one_one {	
+	public static void main(String[] args) {
+		String test_string = "pqwefopqwebfpoqwbef[oqhwepirufhqpouef2-39kbqw";
+		HashMap<Character, Integer> hm = new HashMap<Character, Integer>();
+		System.out.println(test_overlap(hm, test_string));
+	}
+	
+	// iterates across string one char at a time. puts chars into hashmap, then returns as soon as it finds a collision.
+	public static boolean test_overlap(HashMap<Character, Integer> hash, String my_test_string) {
+		for (int i = 0; i < my_test_string.length(); i++) {
+			if (hash.containsKey(my_test_string.charAt(i))){
+				return false;
+			}
+			else {
+				hash.put(my_test_string.charAt(i), 1);
+			}
+		}
+		return true;
+	}
+}
